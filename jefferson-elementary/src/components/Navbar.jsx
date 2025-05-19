af3f6ca99fee442438d04c1efef82cd5d13231ff
myngbruh <71903484+myngbruh@users.noreply.github.com>
2025-05-19 13:59:13 -0700

    home + nav bar done

--- jefferson-elementary/src/components/Navbar.jsx
+++ jefferson-elementary/src/components/Navbar.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import tigerIcon from '../assets/logo.png'
 
 const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/directory">Directory</Link></li>
-                <li><Link to="/calendar">Calendar</Link></li>
-            </ul>
+  const location = useLocation() // This makes it so main nav bar doesn't show in home page
+  const isHome = location.pathname === '/'
+
+  return (
+    <div>
+      <header className="school-header">
+        <img src={tigerIcon} alt="Tiger Logo" className="logo" />
+        <Link to="/" className="school-name-link">
+          <h1>Thomas Jefferson School</h1>
+        </Link>
+      </header>
+
+      {!isHome && (
+        <nav className="main-nav">
+          <ul>
+            <li><Link to="/directory">Directory</Link></li>
+            <li><Link to="/dashboard">Class Dashboard</Link></li>
+            <li><Link to="/calendar">Calendar</Link></li>
+          </ul>
         </nav>
-    )
+      )}
+    </div>
+  )
 }
 
 export default Navbar
