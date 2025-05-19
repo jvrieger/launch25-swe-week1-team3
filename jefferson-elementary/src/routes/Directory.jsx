e2ef899ff006d4f70634b8e66d627c20233944a3
Nick Nguyen <nickn@ad.unc.edu>
2025-05-19 16:10:04 -0400

    initial commit

---
+++ jefferson-elementary/src/routes/Directory.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Directory = () => {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio</h1>
+            <p>Hi, I'm Nick</p>
+        </div>
+    )
+}
+
+export default Directory
